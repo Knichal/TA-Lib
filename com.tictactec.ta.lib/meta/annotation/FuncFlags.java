9dedec5b367ad888518586ac77b4ad50a150ff64
Knichal <knichu93@gmail.com>
2015-06-15 21:05:59 +0200

    Opis FuncFlags

--- com.tictactec.ta.lib/meta/annotation/FuncFlags.java
+++ com.tictactec.ta.lib/meta/annotation/FuncFlags.java
@@ -45,14 +45,27 @@
  */
 
 package com.tictactec.ta.lib.meta.annotation;
-
+/**
+ * Jest to klasa zawieraj¹ca flagi funckji.
+ * @author Komentarz - Maciej Knichal
+ *
+ */
 public class FuncFlags {
-    static public final int TA_FUNC_FLG_OVERLAP     = 0x01000000;   /* Output scale same as input data. */
-    static public final int TA_FUNC_FLG_VOLUME      = 0x04000000;   /* Output shall be over the volume data. */
+    /**
+     * Waga wyjœciowa taka sama jak dane wejsciowe.
+     */
+
+    static public final int TA_FUNC_FLG_OVERLAP     = 0x01000000;   /* Waga wyjœciowa taka sama jak dane wejsciowe. */
+    /**
+     * Wyjœcie powinno przekraczac objetosc danych.
+     */
+    static public final int TA_FUNC_FLG_VOLUME      = 0x04000000;   /* Wyjœcie powinno przekraczac objetosc danych. */
     static public final int TA_FUNC_FLG_UNST_PER    = 0x08000000;   /* Indicate if this function have an unstable 
                                                                      * initial period. Some additional code exist
                                                                      * for these functions for allowing to set that
-                                                                     * unstable period. See Documentation.
+     /**
+     * Wyjœcie powinno byc candlestick.
+     */                                                                  * unstable period. See Documentation.
                                                                      */
-    static public final int TA_FUNC_FLG_CANDLESTICK = 0x10000000;   /* Output shall be a candlestick */
+    static public final int TA_FUNC_FLG_CANDLESTICK = 0x10000000;   /* Wyjœcie powinno byc candlestick. */
 }
